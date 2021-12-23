a07af41c05704943e411c09a94db4bcc59ec8758
dali <lionelviperdali@gmail.com>
2021-12-23 17:44:29 +0100

    * fix error build

--- dart-ffi/src/instance.rs
+++ dart-ffi/src/instance.rs
@@ -1,41 +1,48 @@
+use std::fmt::Error;
+use std::ops::Deref;
 use ble_desktop::common::utils::*;
 use ble_desktop::models::ble_core::{BleCore, BleRepo};
-use crate::utils::{run_async, RUNTIME_THREAD, runtime, error};
+use crate::{runtime, utils::run_async, error};
 use allo_isolate::Isolate;
+use futures::executor::block_on;
+use std::sync::{Arc};
+use std::marker::{Sync, Send};
+use crate::utils::RUNTIME_THREAD;
 
 struct BleCoreSend(*mut *const BleCore);
 
 unsafe impl Send for BleCoreSend {}
+
+unsafe impl Sync for BleCoreSend {}
 
 #[no_mangle]
 pub unsafe extern "C" fn ble_instance(
     ble: *mut *const BleCore
 ) {
     let ble_core_send = BleCoreSend(ble);
-    async fn open() -> Result<Arc<BleCore>> {
-        let instance = BleCore::create().await;
-        Ok(instance)
-    }
     run_async(move || {
-        let ble = ble_core_send;
-        match open() {
-            Ok(instance) => {
-                ble.0.write(instance.as_ref());
-            }
-            Err(e) => {
-                panic!("error to intantiate ble core")
-            }
+        async {
+            BleCore::create().await;
+        };
+    });
+    let instance = BleCore::get_instance();
+    match instance {
+        Some(bleCore) => {
+            ble_core_send.0.write(bleCore.as_ref());
         }
-    })
+        _ => {
+            panic!("error to intantiate ble core")
+        }
+    }
 }
 
 pub unsafe extern "C" fn get_list_devices(port: i64, seconds: u64) {
-    let ble = BleCore::get_instance().unwrap();
+    let mut ble = BleCore::get_instance().unwrap().deref().clone();
     let rt = runtime!();
     rt.spawn(async move {
-        let devices = ble.list_devices(seconds).await;
+        let devices = (ble.list_devices(Some(seconds))).await;
         let isolate = Isolate::new(port);
         let json = map_device_to_json(devices);
-        isolate.post(json)
-    })
+        isolate.post(json);
+    });
 }
